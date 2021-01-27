f30d879dd91c103004b6933d24495866d6a20d14
Bogdan <tischenkob@gmail.com>
2021-01-27 10:27:07 +0300

    comment out name and scores fields to pass test one

--- hw02/src/main/java/homework/Customer.java
+++ hw02/src/main/java/homework/Customer.java
@@ -5,7 +5,7 @@ public class Customer {
     private String name;
     private long scores;
 
-    //todo: 1. в этом классе надо исправить ошибки
+    //TODO: 1. в этом классе надо исправить ошибки
 
     public Customer(long id, String name, long scores) {
         this.id = id;
@@ -50,15 +50,16 @@ public class Customer {
         Customer customer = (Customer) o;
 
         if (id != customer.id) return false;
-        if (scores != customer.scores) return false;
-        return name != null ? name.equals(customer.name) : customer.name == null;
+        //if (scores != customer.scores) return false;
+        //return name != null ? name.equals(customer.name) : customer.name == null;
+        return true;
     }
 
     @Override
     public int hashCode() {
         int result = (int) (id ^ (id >>> 32));
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + (int) (scores ^ (scores >>> 32));
+        //result = 31 * result + (name != null ? name.hashCode() : 0);
+        //result = 31 * result + (int) (scores ^ (scores >>> 32));
         return result;
     }
 }

074f7ffee88e31d251b242aad83f88f6771bda71
Bogdan <tischenkob@gmail.com>
2021-01-27 10:28:08 +0300

    implement methods with a deque

--- hw02/src/main/java/homework/CustomerReverseOrder.java
+++ hw02/src/main/java/homework/CustomerReverseOrder.java
@@ -1,16 +1,20 @@
 package homework;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class CustomerReverseOrder {
 
-    //todo: 2. надо реализовать методы этого класса
+    //TODO: 2. надо реализовать методы этого класса
     //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
 
-    public void add(Customer customer) {
+    private Deque<Customer> dequeOfCustomers = new ArrayDeque<>();
 
+    public void add(Customer customer) {
+       dequeOfCustomers.addLast(customer);
     }
 
     public Customer take() {
-        return null; // это "заглушка, чтобы скомилировать"
+        return dequeOfCustomers.removeLast();
     }
 }

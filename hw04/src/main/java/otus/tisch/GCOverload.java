3a3463cdec55578b7aac45077747f51013023abc
Bogdan Tischenko <tischenkob@gmail.com>
2021-03-09 16:47:43 +0300

    add overload logic implementation

--- hw04/src/main/java/otus/tisch/GCOverload.java
+++ hw04/src/main/java/otus/tisch/GCOverload.java
@@ -15,5 +15,49 @@
 
 package otus.tisch;
 
+import java.util.*;
+
 public class GCOverload {
+    public static void main(String[] args) {
+        Set<Object> set = new HashSet<>();
+        Map<Object> map = new HashMap();
+
+        List<Person> people = new ArrayList<>();
+
+        while (true) {
+            for (int i = 0; i < 1000; i++) {
+                people.add(new Person());
+            }
+            for (int i = 0; i < 500; i++) {
+                people.remove(i);
+            }
+        }
+    }
+
+    static class Person {
+        private final String name;
+        private final Integer age;
+
+        Person() {
+            this.age = new Random().nextInt();
+            name = randomName();
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        private static String randomName() {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < 10; i++) {
+                var letter = (char) (new Random()).nextInt();
+                result.append(letter);
+            }
+            return result.toString();
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
 }

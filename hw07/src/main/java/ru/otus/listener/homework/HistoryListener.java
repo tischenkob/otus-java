5d9ee8872d0589b2ebc5cb828bb0bb3d33ae480b
Bogdan <tischenkob@gmail.com>
2021-06-10 19:33:34 +0300

    Make map private final and move copy logic

--- hw07/src/main/java/ru/otus/listener/homework/HistoryListener.java
+++ hw07/src/main/java/ru/otus/listener/homework/HistoryListener.java
@@ -2,7 +2,6 @@ package ru.otus.listener.homework;
 
 import ru.otus.listener.Listener;
 import ru.otus.model.Message;
-import ru.otus.model.ObjectForMessage;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -10,18 +9,14 @@ import java.util.Optional;
 
 public class HistoryListener implements Listener, HistoryReader {
 
-    Map<Long, Message> history = new HashMap<>();
+    private final Map<Long, Message> history = new HashMap<>();
 
     @Override
     public void onUpdated(Message msg) {
-        // Так как в сигнатуре привязываемся к Message, то и какое-то хитрое глубокое
-        // копирование делать нет смысла
-        ObjectForMessage dataCopy = new ObjectForMessage();
-        dataCopy.setData(msg.getField13().getData());
-
-        Message copy = msg.toBuilder().field13(dataCopy).build();
+        var copy = Message.copyOf(msg);
         history.put(msg.getId(), copy);
     }
+
 
     @Override
     public Optional<Message> findMessageById(long id) {

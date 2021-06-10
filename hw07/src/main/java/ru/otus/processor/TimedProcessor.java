d41fb8ebe65d19f553c292ce5c04c752cb145dd9
Bogdan <tischenkob@gmail.com>
2021-06-10 19:34:01 +0300

    Add time supplier dependency injection and Time interface

--- hw07/src/main/java/ru/otus/processor/TimedProcessor.java
+++ hw07/src/main/java/ru/otus/processor/TimedProcessor.java
@@ -2,14 +2,25 @@ package ru.otus.processor;
 
 import ru.otus.model.Message;
 
-import java.time.LocalTime;
+public class TimedProcessor implements Processor {
 
-public class TimedProcessor implements Processor{
+    private final Time time;
+
+    public TimedProcessor(Time instance) {
+        time = instance;
+    }
+
     @Override
     public Message process(Message message) {
-        if (LocalTime.now().getSecond() % 2 == 0) {
+        if (time.getSeconds() % 2 == 0) {
             throw new IllegalStateException("You cannot use this processor during an even second.");
         }
         return message;
     }
+}
+
+interface Time {
+
+    int getSeconds();
+
 }

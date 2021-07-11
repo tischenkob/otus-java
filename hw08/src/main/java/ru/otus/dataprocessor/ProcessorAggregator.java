cf51904796f6211c2a814e4ea6b8f99fb09cdf26
Bogdan Tischenko <tischenkob@gmail.com>
2021-07-11 17:16:57 +0300

    Implementation

--- hw08/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
+++ hw08/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
@@ -2,14 +2,25 @@ package ru.otus.dataprocessor;
 
 import ru.otus.model.Measurement;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.summingDouble;
+
 
 public class ProcessorAggregator implements Processor {
 
     @Override
     public Map<String, Double> process(List<Measurement> data) {
-        // TODO: группирует выходящий список по name, при этом суммирует поля value
-        return null;
+        return data.stream()
+                .collect(
+                        groupingBy(
+                                Measurement::getName,
+                                LinkedHashMap::new,
+                                summingDouble(Measurement::getValue)
+                        )
+                );
     }
 }

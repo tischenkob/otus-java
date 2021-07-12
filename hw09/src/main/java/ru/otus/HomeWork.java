a9f407a24db44118b4588a07ff4809a7eed917f9
Bogdan Tischenko <tischenkob@gmail.com>
2021-07-12 07:27:45 +0300

    Init

--- hw09/src/main/java/ru/otus/HomeWork.java
+++ hw09/src/main/java/ru/otus/HomeWork.java
@@ -10,9 +10,9 @@ import ru.otus.crm.model.Client;
 import ru.otus.crm.model.Manager;
 import ru.otus.crm.service.DbServiceClientImpl;
 import ru.otus.crm.service.DbServiceManagerImpl;
+import ru.otus.jdbc.mapper.DataTemplateJdbc;
 import ru.otus.jdbc.mapper.EntityClassMetaData;
 import ru.otus.jdbc.mapper.EntitySQLMetaData;
-import ru.otus.jdbc.mapper.DataTemplateJdbc;
 
 import javax.sql.DataSource;
 

1b6853e437c39d2e8321b77d66274e75cbd492f5
Bogdan Tischenko <tischenkob@gmail.com>
2021-07-18 14:07:39 +0300

    Add @Id

--- hw09/demo/src/main/java/ru/otus/crm/model/Client.java
+++ hw09/demo/src/main/java/ru/otus/crm/model/Client.java
@@ -1,6 +1,9 @@
 package ru.otus.crm.model;
 
+import ru.otus.core.Id;
+
 public class Client {
+    @Id
     private Long id;
     private String name;
 
--- hw09/demo/src/main/java/ru/otus/crm/model/Manager.java
+++ hw09/demo/src/main/java/ru/otus/crm/model/Manager.java
@@ -1,6 +1,9 @@
 package ru.otus.crm.model;
 
+import ru.otus.core.Id;
+
 public class Manager {
+    @Id
     private Long no;
     private String label;
 

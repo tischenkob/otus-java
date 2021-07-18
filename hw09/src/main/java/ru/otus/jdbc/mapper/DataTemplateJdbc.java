afa3f680a8bcc37ad3cac257c4772726cac0fd52
Bogdan Tischenko <tischenkob@gmail.com>
2021-07-18 14:09:39 +0300

    Complete homework

--- hw09/src/main/java/ru/otus/jdbc/mapper/DataTemplateJdbc.java
+++ hw09/src/main/java/ru/otus/jdbc/mapper/DataTemplateJdbc.java
@@ -3,9 +3,18 @@ package ru.otus.jdbc.mapper;
 import ru.otus.core.repository.DataTemplate;
 import ru.otus.core.repository.executor.DbExecutor;
 
+import java.lang.reflect.Field;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
+
+import static java.util.Collections.emptyList;
+import static java.util.Collections.unmodifiableList;
+import static java.util.stream.Collectors.toList;
+import static ru.otus.core.sessionmanager.TransactionRunnerJdbc.wrapException;
 
 /**
  * Сохратяет объект в базу, читает объект из базы
@@ -14,29 +23,64 @@ public class DataTemplateJdbc<T> implements DataTemplate<T> {
 
     private final DbExecutor dbExecutor;
     private final EntitySQLMetaData entitySQLMetaData;
+    private final EntityClassMetaData<T> classMetaData;
+    private final Function<ResultSet, T> resultSetToEntity;
 
-    public DataTemplateJdbc(DbExecutor dbExecutor, EntitySQLMetaData entitySQLMetaData) {
+    public DataTemplateJdbc(DbExecutor dbExecutor,
+                            EntitySQLMetaData entitySQLMetaData,
+                            EntityClassMetaData<T> classMetaData) {
         this.dbExecutor = dbExecutor;
         this.entitySQLMetaData = entitySQLMetaData;
+        this.classMetaData = classMetaData;
+
+        this.classMetaData.getAllFields().forEach(field -> field.setAccessible(true)); // или лучше геттер искать?
+
+        this.resultSetToEntity = resultSet -> wrapException(() -> {
+            Function<Field, Object> fieldToEntity = field -> wrapException(() ->
+                    resultSet.getObject(field.getName(), field.getType()));
+            Object[] fieldValues = this.classMetaData.getAllFields().stream().map(fieldToEntity).toArray();
+            return classMetaData.getConstructor().newInstance(fieldValues);
+        });
     }
 
     @Override
     public Optional<T> findById(Connection connection, long id) {
-        throw new UnsupportedOperationException();
+        String sql = entitySQLMetaData.getSelectByIdSql();
+        return dbExecutor.executeSelect(connection, sql, List.of(id),
+                resultSet -> wrapException(() -> {
+                    if (!resultSet.next()) return null;
+                    return resultSetToEntity.apply(resultSet);
+                }));
     }
 
     @Override
     public List<T> findAll(Connection connection) {
-        throw new UnsupportedOperationException();
+        String sql = entitySQLMetaData.getSelectAllSql();
+        return dbExecutor.executeSelect(connection, sql, emptyList(),
+                resultSet -> wrapException(() -> {
+                    List<T> entities = new ArrayList<>();
+                    while (resultSet.next()) entities.add(resultSetToEntity.apply(resultSet));
+                    return unmodifiableList(entities);
+                })).orElse(emptyList());
     }
 
     @Override
-    public long insert(Connection connection, T client) {
-        throw new UnsupportedOperationException();
+    public long insert(Connection connection, T entity) {
+        String sql = entitySQLMetaData.getInsertSql();
+        List<Object> values = getFieldsWithoutIdValues(entity);
+        return dbExecutor.executeStatement(connection, sql, values);
     }
 
     @Override
-    public void update(Connection connection, T client) {
-        throw new UnsupportedOperationException();
+    public void update(Connection connection, T entity) {
+        String sql = entitySQLMetaData.getUpdateSql();
+        List<Object> values = getFieldsWithoutIdValues(entity);
+        values.add(wrapException(() -> this.classMetaData.getIdField().get(entity)));
+        dbExecutor.executeStatement(connection, sql, values);
+    }
+
+    private List<Object> getFieldsWithoutIdValues(T entity) {
+        Function<Field, Object> fieldToValue = field -> wrapException(() -> field.get(entity));
+        return this.classMetaData.getFieldsWithoutId().stream().map(fieldToValue).collect(toList());
     }
 }
---
+++ hw09/src/main/java/ru/otus/jdbc/mapper/EntityClassMetaDataImpl.java
@@ -0,0 +1,66 @@
+package ru.otus.jdbc.mapper;
+
+import ru.otus.core.Id;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static java.util.Objects.requireNonNullElseGet;
+import static java.util.stream.Collectors.toUnmodifiableList;
+
+public class EntityClassMetaDataImpl<T> implements EntityClassMetaData<T> {
+    private final Class<T> entityClass;
+    private Constructor<T> constructor;
+    private Field idField;
+    private List<Field> fields;
+
+    public EntityClassMetaDataImpl(Class<T> instance) {
+        entityClass = instance;
+    }
+
+    @Override
+    public String getName() {
+        return entityClass.getSimpleName();
+    }
+
+    @Override
+    public Constructor<T> getConstructor() {
+        constructor = requireNonNullElseGet(constructor, () -> {
+            try {
+                Class<?>[] types = getAllFields().stream()
+                        .map(Field::getType)
+                        .toArray(Class[]::new);
+                return entityClass.getConstructor(types);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+        return constructor;
+    }
+
+    @Override
+    public Field getIdField() {
+        Predicate<Field> withIdAnnotation = f -> f.getAnnotation(Id.class) != null;
+        idField = requireNonNullElseGet(idField, () -> getAllFields().stream()
+                .filter(withIdAnnotation)
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException(String.format("Class %s doesn't have @Id", getName()))));
+        return idField;
+    }
+
+    @Override
+    public List<Field> getAllFields() {
+        fields = requireNonNullElseGet(fields, () -> Arrays.asList(entityClass.getDeclaredFields()));
+        return Collections.unmodifiableList(fields);
+    }
+
+    @Override
+    public List<Field> getFieldsWithoutId() {
+        Predicate<Field> withoutIdAnnotation = f -> !f.equals(getIdField());
+        return getAllFields().stream().filter(withoutIdAnnotation).collect(toUnmodifiableList());
+    }
+}

7bcbcfcd860dfb3f8a85ce48e705a25df21ca257
Bogdan Tischenko <tischenkob@gmail.com>
2021-09-21 22:13:26 +0300

    Migrate to otus build.gradle

---
+++ buildSrc/src/main/java/Versions.java
@@ -0,0 +1,36 @@
+public interface Versions {
+    String guava = "30.1.1-jre";
+    String jmh = "1.27";
+    String jol = "0.15";
+    String asm = "9.1";
+    String glassfishJson = "1.1.4";
+    String protobuf = "3.12.1";
+
+    String flyway = "6.4.4";
+    String postgresql = "42.2.5";
+    String testcontainers = "1.15.3";
+
+
+    String lombok = "1.18.20";
+    String gson = "2.8.7";
+
+    String mongodb = "4.2.3";
+    String mongodbReactive = "4.2.3";
+    String cassandra = "4.12.0";
+    String neo4j = "4.3.3";
+    String jedis = "3.6.1";
+
+    String jetty = "11.0.6";
+    String freemarker = "2.3.31";
+
+    String reflections = "0.9.12";
+
+    String sockjs = "1.1.2";
+    String stomp = "2.3.3-1";
+    String bootstrap = "4.4.1-1";
+
+    String jsr305 = "3.0.2";
+
+    String grpc = "1.39.0";
+    String netty = "4.1.53.Final";
+}

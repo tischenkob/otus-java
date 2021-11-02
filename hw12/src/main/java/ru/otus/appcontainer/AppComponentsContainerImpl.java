64e3d0556c6ea22ec71d8fb28db9b285f56c0dfe
Bogdan Tischenko <b.tishchenko@tinkoff.ru>
2021-11-02 13:03:12 +0300

    Обработка конфигов в правильном порядке, проверка на null и проч.

--- hw12/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
+++ hw12/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -3,7 +3,6 @@ package ru.otus.appcontainer;
 import static java.util.Comparator.comparingInt;
 import static java.util.stream.Collectors.toList;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.util.ArrayList;
@@ -11,7 +10,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Predicate;
 import lombok.SneakyThrows;
 import org.reflections.Reflections;
 import org.reflections.scanners.SubTypesScanner;
@@ -25,28 +23,18 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
     private final List<Object> appComponents = new ArrayList<>();
     private final Map<String, Object> appComponentsByName = new HashMap<>();
 
-    public AppComponentsContainerImpl(Class<?>... initialConfigClass) {
-        Arrays.stream(initialConfigClass).forEach(this::processConfig);
+    public AppComponentsContainerImpl(Class<?>... configClasses) {
+        processConfigsInOrder(configClasses);
     }
 
     public AppComponentsContainerImpl(String packageName) {
-        Reflections configScanner = new Reflections(packageName,
-                new SubTypesScanner(),
-                new TypeAnnotationsScanner());
-        var configClasses = configScanner.getTypesAnnotatedWith(
-                AppComponentsContainerConfig.class);
-        configClasses.forEach(this::processConfig);
+        Class<?>[] configClasses = findConfigurationClasses(packageName);
+        processConfigsInOrder(configClasses);
     }
 
     @Override
     public <C> C getAppComponent(Class<C> componentClass) {
-        final Predicate<Object> hasTargetClassOrInterface =
-                c -> c.getClass().equals(componentClass) ||
-                        Arrays.asList(c.getClass().getInterfaces()).contains(componentClass);
-        final Object component = appComponents.stream()
-                .filter(hasTargetClassOrInterface)
-                .findFirst()
-                .orElse(null);
+        Object component = findComponentAssignableFrom(componentClass);
         return componentClass.cast(component);
     }
 
@@ -55,19 +43,14 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
         return (C) appComponentsByName.get(componentName);
     }
 
-    private static boolean isAppComponent(Method m) {
-        return m.isAnnotationPresent(AppComponent.class);
-    }
-
     @SneakyThrows
     private void processConfig(Class<?> configClass) {
         checkConfigClass(configClass);
-        // You code here...
         Object configInstance = configClass.getConstructor().newInstance();
         List<Method> annotatedMethods = Arrays.stream(configClass.getDeclaredMethods())
-                .filter(AppComponentsContainerImpl::isAppComponent)
-                .sorted(comparingInt(this::getComponentOrder))
-                .collect(toList());
+                                              .filter(this::isAppComponent)
+                                              .sorted(comparingInt(this::getComponentOrder))
+                                              .collect(toList());
 
         for (Method method : annotatedMethods) {
             Object component = buildComponent(configInstance, method);
@@ -76,23 +59,64 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
         }
     }
 
-    @SneakyThrows
-    private Object buildComponent(Object config, Method method) {
-        final Object[] componentArguments = Arrays.stream(method.getParameters())
-                .map(Parameter::getType)
-                .map(this::getAppComponent)
-                .toArray();
-        return method.invoke(config, componentArguments);
+    private void processConfigsInOrder(Class<?>[] configClasses) {
+        Class<?>[] sortedConfigs = Arrays.copyOf(configClasses, configClasses.length);
+        Arrays.sort(sortedConfigs, comparingInt(this::getConfigOrder));
+        for (Class<?> config : sortedConfigs) {
+            processConfig(config);
+        }
     }
 
-    private int getComponentOrder(Method m) {
-        return m.getAnnotation(AppComponent.class).order();
+    private Class<?>[] findConfigurationClasses(String packageName) {
+        Reflections configScanner = new Reflections(packageName,
+                                                    new SubTypesScanner(),
+                                                    new TypeAnnotationsScanner());
+        return configScanner
+                .getTypesAnnotatedWith(AppComponentsContainerConfig.class)
+                .toArray(new Class[]{});
+    }
+
+    private <C> Object findComponentAssignableFrom(Class<C> componentClass) {
+        for (Object component : appComponents) {
+            if (componentClass.isAssignableFrom(component.getClass())) {
+                return component;
+            }
+        }
+        return null;
+    }
+
+    @SneakyThrows
+    private Object buildComponent(Object config, Method method) {
+        Parameter[] parameters = method.getParameters();
+        var arguments = new Object[parameters.length];
+        for (int i = 0; i < parameters.length; i++) {
+            Object appComponent = getAppComponent(parameters[i].getType());
+            if (appComponent == null) {
+                throw new RuntimeException("Component not found for type " +
+                                           parameters[i].getType());
+            }
+            arguments[i] = appComponent;
+        }
+        return method.invoke(config, arguments);
+    }
+
+    // Эти методы не static, чтобы ссылка на них не требовала длинного имени класса
+    private int getComponentOrder(Method method) {
+        return method.getAnnotation(AppComponent.class).order();
+    }
+
+    private int getConfigOrder(Class<?> config) {
+        return config.getAnnotation(AppComponentsContainerConfig.class).order();
+    }
+
+    private boolean isAppComponent(Method m) {
+        return m.isAnnotationPresent(AppComponent.class);
     }
 
     private void checkConfigClass(Class<?> configClass) {
         if (!configClass.isAnnotationPresent(AppComponentsContainerConfig.class)) {
-            throw new IllegalArgumentException(
-                    String.format("Given class is not in config %s", configClass.getName()));
+            throw new IllegalArgumentException("Given class is not in config " +
+                                               configClass.getName());
         }
     }
 }
